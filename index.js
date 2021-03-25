bf9461acd173971dddcad6f1237d1c9ed80fbc57
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-26 00:23:57 +1100

    Doen with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+function moveDodgerLeft(){
+    let leftN = dodger.style.left.replace('px', '');
+    let leftInteger = parseInt(leftN, 10);
+    dodger.style.left = `${leftInteger-1}px`;
+}
+
+function moveDodgerRight(){
+    let leftN = dodger.style.left.replace('px', '');
+    let leftInteger = parseInt(leftN, 10);
+    dodger.style.left = `${leftInteger+1}px`;
+}
